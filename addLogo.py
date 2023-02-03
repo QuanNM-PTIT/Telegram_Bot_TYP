f8c267d74431d025d56f5786a9f68dc3381f12f8
Jimmy Nguyễn <quannm.b20cn550@stu.ptit.edu.vn>
2023-02-03 23:57:18 +0700

    Update

--- addLogo.py
+++ addLogo.py
@@ -12,29 +12,29 @@ def processLogo():
     img = Image.open("output/output.jpg")
     img_w, img_h = map(int, img.size)
 
-    logo.thumbnail((min(img.size) // 5, min(img.size) // 5))
+    logo.thumbnail((max(img.size) // 5, min(img.size) // 4))
     water_mark_w, water_mark_h = map(int, logo.size)
 
     # Căn phải dưới
-    x = img_w - water_mark_w + img_w // 50
-    y = img_h - water_mark_h + img_h // 50
+    x = img_w - water_mark_w - img_w // 40
+    y = img_h - water_mark_h - img_h // 40
 
     addLogo(x, y, "bottom_right")
 
     # Căn trái dưới
-    x = - img_w // 50
-    y = img_h - water_mark_h + img_h // 50
+    x = img_w // 40
+    y = img_h - water_mark_h - img_h // 40
 
     addLogo(x, y, "bottom_left")
 
     # Căn phải trên
-    x = img_w - water_mark_w + img_w // 50
-    y = - img_h // 50
+    x = img_w - water_mark_w - img_w // 40
+    y = img_h // 40
 
     addLogo(x, y, "upper_right")
 
     # Căn trái trên
-    x = - img_w // 50
-    y = - img_h // 50
+    x = img_w // 40
+    y = img_h // 40
 
     addLogo(x, y, "upper_left")
